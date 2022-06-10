e78b9e8626034ea41ab6febab35e154708cbf893
Facundo Mangioni <106379788+FacuMangi@users.noreply.github.com>
2022-06-10 14:24:26 -0300

    Topics page

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -1,4 +1,4 @@
-"""Defines URL patterns for learning_logs."""
+"""Defines URL patterns for learning_log."""
 
 from django.urls import path
 
@@ -8,4 +8,6 @@ app_name = 'learning_log'
 urlpatterns = [
 	#Home page
 	path('', views.index, name='index'),
+	#Page that shows all topics.
+	path('topics/', views.topics, name='topics')
 ]
